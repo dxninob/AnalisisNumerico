b3faa6d3a7be704adf4a9ec6a13595a9703d47b2
dxninob <60080916+dxninob@users.noreply.github.com>
2021-10-02 03:23:35 -0500

    Se subió el código de punto fijo en Python

---
+++ python/puntoFijo.py
@@ -0,0 +1,30 @@
+from math import log
+
+def g(x):
+    return log(x**2 - 2*x +2)
+
+
+x0 = float(input('Ingrese el X0: '))
+tol = float(input('Ingrese la tolerancia: '))
+maxIter = int(input('Ingrese el número máximo de iteraciones: '))
+
+cont = 0
+error = tol + 1
+
+while True:
+    gx0 = g(x0)
+    if error <= tol:
+        print('Existe la raiz', x0, 'con tolerancia de', tol)
+        print(error)
+        print('g(x) = ', gx0)
+        print('Iteración:', cont - 1)
+        break
+    
+    if cont == maxIter:
+        print('No se encontró una solución')
+        break
+    
+    xn = gx0
+    error =  abs(xn-x0)
+    x0 = xn
+    cont += 1
