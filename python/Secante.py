22fb97592d7c3bd3fe4adc38ed741bb9ba9a883a
asepulvede <60146746+asepulvede@users.noreply.github.com>
2021-10-10 18:07:44 -0500

    Create Secante.py

---
+++ python/Secante.py
@@ -0,0 +1,18 @@
+!pip install import sympy as sp
+from sympy import *
+import math
+from math import *
+x= symbols('x')sympy
+def secante(fx,x0,xf,maxI,tol):
+  cont=0
+  error=tol+1
+  while cont<maxI and error>tol:
+    xn= x0-fx.subs(x,x0)*(xf-x0)/(fx.subs(x,xf)-fx.subs(x,x0))
+    error= abs(xn-x0)
+    xf=x0
+    x0=xc
+    cont= cont+1
+  if error<tol:
+    print(xn,'es raiz con tolerancia',tol)
+  else:
+    print('no hay raiz')
