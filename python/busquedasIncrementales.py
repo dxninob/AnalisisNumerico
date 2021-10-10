d9f3edf058597bfdd3ea0d5caafbae3ef6ab2336
asepulvede <60146746+asepulvede@users.noreply.github.com>
2021-10-10 17:01:38 -0500

    Update busquedasIncrementales.py

--- python/busquedasIncrementales.py
+++ python/busquedasIncrementales.py
@@ -1,37 +1,24 @@
-def f(x):
-    return -90 * (x-45) * (x+40) * (x-27) * (x+95) - 6000000
+!pip install sympy
+import sympy as sp
+from sympy import *
+import math
+from math import *
+x= symbols('x')
+def busquedas_in(fx,x0,cx,maxI):
+  if fx.subs(x,x0)==0:
+    print(x0, 'Es raiz')
+  else: 
+    cont= 0
+    x1= x0+cx
+  while cont<maxI and float(fx.subs(x,x0)*fx.subs(x,x1))>0:
+    cont= cont+1
+    x0= x1 
+    x1= x0+cx
+  if fx.subs(x,x1)==0:
+    print(x1, 'Es raiz')
+  elif fx.subs(x,x0)*fx.subs(x,x1)<0:
+    print('Entre', x0, 'y', x1, 'hay al menos una raiz')
+  else: 
+    print('no hay solución')
 
-
-x0 = float(input('Ingrese el X0: '))
-cx = float(input('Ingrese el cambio de X: '))
-maxIter = int(input('Ingrese el número máximo de iteraciones: '))
-
-fx0 = f(x0)
-if fx0 == 0:
-    print('Existe la raiz', x0)
-    print('Iteración: 0')
-
-else:
-    cont = 1
-    x1 = x0 + cx
-    while True:
-        fx1 = f(x1)
-        if fx1 == 0:
-            print('Existe la raiz', x1)
-            print('Iteración:', cont)
-            break
-        
-        fx0 = f(x0)
-        if fx0 * fx1 < 0:
-            print('Existe al menos una raiz entre', x0, 'y', x1)
-            print('f(X0) =', fx0, '- f(X1) =', fx1)
-            print('Iteraciónes:', cont - 1, ',', cont)
-            break
-
-        if cont == maxIter:
-            print('No se encontró una solución')
-            break
-
-        x0 = x1
-        x1 = x0 + cx
-        cont += 1
+busquedas_in(x-1,-1,0.5,20) 
